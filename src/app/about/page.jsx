9571a8eb624efa142d8956e1e198fbfd00892f3f
malik-atlas <malikux@proton.me>
2026-06-12 15:06:56 +0000

    text changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,21 +10,14 @@ const AboutPage = () => {
     <>
       <PageIntro eyebrow="About us" title="Our strength is collaboration">
         <p>
-          Malik Smith - We believe that our strength lies in our collaborative approach, which
-          puts our clients at the center of everything we do.
+         My design philosophy has always been "strive for creative clarity." When designers choose the right lens to empathize with users, product experiences become moments of functional delight and a true enhancement to their daily lives. 
         </p>
         <div className="mt-10 max-w-2xl space-y-6 text-base">
           <p>
-            Studio was started by three friends who noticed that developer
-            studios were charging clients double what an in-house team would
-            cost. Since the beginning, we have been committed to doing things
-            differently by charging triple instead.
+            Blurb 1
           </p>
           <p>
-            At Studio, we’re more than just colleagues — we’re a family. This
-            means we pay very little and expect people to work late. We want our
-            employees to bring their whole selves to work. In return, we just
-            ask that they keep themselves there until at least 6:30pm.
+           Blurb 2
           </p>
         </div>
       </PageIntro>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,12 +12,11 @@ export default function Home() {
       <Container className="mt-24 sm:mt-32">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Make your online presence a reality with us! - HELLO WORLD!
+            Malik Smith, Design Engineer
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            Malik Smith - We are a group of developer working at the intersection of design
-            and technology. It is a really busy intersection though — a lot of
-            our staff have been involved in hit and runs.
+            Malik Smith is a design engineer working at the intersection of design
+            and technology. With a background in human computer interaction and UX / Product Design, the goal is always to lead with empathy and maximizing human potential. 
           </p>
         </FadeIn>
       </Container>

56af71503e90f8131f7c7aa11cb7f7974ea6fa4f
malik-atlas <malikux@proton.me>
2026-06-13 18:04:34 +0000

    updated text

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,16 +10,16 @@ const AboutPage = () => {
     <>
       <PageIntro eyebrow="About me" title="My strength is collaboration">
         <p>
-         My design philosophy has always been  &quot;strive for creative clarity. &quot; 
+         My design philosophy has always been  &quot;strive for creative clarity.&quot; 
          When designers choose the right lens to empathize with users, product experiences
           become moments of functional delight and a true enhancement to their daily lives.
         </p>
         <div className="mt-10 max-w-2xl space-y-6 text-base">
           <p>
-            For over 15 years&comma; I&apos;ve helped cross-functional teams deliver human-centered enterprise and consumer products, apps and websites at scale. My passion for technology, teams and innovation motivate me to keep striving forward. 
+            For over 15 years, I&apos;ve helped cross-functional teams deliver human-centered enterprise and consumer products, apps and websites at scale. My passion for technology, teams and innovation motivate me to keep striving forward. 
           </p>
           <p>
-          Throughout my career&comma; I have prided myself on fostering an environment of effective cross-collaboration and a joy for good design and humanity.
+          Throughout my career, I have prided myself on fostering an environment of effective cross-collaboration and a joy for good design and humanity.
           </p>
         </div>
       </PageIntro>

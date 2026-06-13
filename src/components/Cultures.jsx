9c8660a3c1b2b4ff5d5f5fc520ea5ae8fd9a6647
malik-atlas <malikux@proton.me>
2026-06-13 20:46:06 +0000

    about text updates

--- src/components/Cultures.jsx
+++ src/components/Cultures.jsx
@@ -7,27 +7,24 @@ const Cultures = () => {
   return (
     <div className="mt-24 rounded-4xl bg-neutral-950 py-24 sm:mt-32 lg:mt-40 lg:py-32">
       <SectionIntro
-        eyebrow="Our culture"
-        title="Balance your passion with your passion for life."
+        eyebrow="My culture"
+        title="Balancing passion for technology with a passion for life."
         invert
       >
         <p>
-          We are a group of like-minded people who share the same core values.
+          I operate on a few core values that are simple but powerful. 
         </p>
       </SectionIntro>
       <Container className="mt-16">
         <GridList>
-          <GridListItem title="Loyalty" invert>
-            Our team has been with us since the beginning because none of them
-            are allowed to have LinkedIn profiles.
+          <GridListItem title="Empathy" invert>
+            By understanding the mental models of customers, I can empathize more accurately. 
           </GridListItem>
-          <GridListItem title="Trust" invert>
-            We don’t care when our team works just as long as they are working
-            every waking second.
+          <GridListItem title="Technical Constraints" invert>
+            Working closely with engineering leads, I am able to deliver functional design specs from day one. 
           </GridListItem>
-          <GridListItem title="Compassion" invert>
-            You never know what someone is going through at home and we make
-            sure to never find out.
+          <GridListItem title="Teamwork" invert>
+            Nothing gets built in a silo so it's important to align with stakeholders on how disparate prioirites can still intersect at a single north star vision. 
           </GridListItem>
         </GridList>
       </Container>
